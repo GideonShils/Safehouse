c1f88b0b5faef7b1eeb273e75bda84b684828dfb
Eli Sherman <esherma@umich.edu>
2017-06-13 19:01:35 +0300

    ML bullshit

---
+++ handle_new_request.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.svm import LinearSVC
+import pickle
+import datetime as datetime
+from sklearn.preprocessing import OneHotEncoder
+import pymysql
+
+def main():
+	
+
+if __name__ == '__main__':
+	main()
