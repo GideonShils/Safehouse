efc4beb4a28d83cbb47baf30ecb29710929ad491
Jimmy Gozal <jgozal@developerkit.co>
2017-06-13 12:35:52 +0200

    fixed random code bugs

--- routes/requestHandlers.js
+++ routes/requestHandlers.js
@@ -1,16 +1,19 @@
 'use strict'
 
 let sms = require('../utils/sms');
+let randomCode;
 
 let auth = function (req, res) {
 
     if (req.body.auth === 1 && req.body.code === 1365) {
-        sms.sendSMS();
+        randomCode = sms.sendSMS();
         res.send(JSON.stringify({ result: 'orange' }));
-    } else if (req.body.auth === 2 && req.body.code === sms.randomCode) {
+    } else if (req.body.auth === 2 && req.body.code === randomCode) {
         res.send(JSON.stringify({ result: 'green' }));
+        randomCode = undefined; //reset
     } else {
         res.send(JSON.stringify({ result: 'red' }));
+        randomCode = undefined; //reset
     }
 }
 
--- utils/sms.js
+++ utils/sms.js
@@ -5,19 +5,19 @@ let authToken = secrets.authToken;
 
 let twilio = require('twilio');
 let client = new twilio(accountSid, authToken);
-let randomCode = Math.floor(1000 + Math.random() * 9000);
 
 let sendSMS = function () {
+    randomCode = Math.floor(1000 + Math.random() * 9000);
     client.messages.create({
         body: 'Your code is ' + randomCode,
         to: '+19293442441',  // Text this number
         from: '+15207292514' // From a valid Twilio number
     })
         .then((message) => console.log(message.sid));
+        return randomCode;
 }
 
 let sms =  {
-    randomCode,
     sendSMS
 }
 
