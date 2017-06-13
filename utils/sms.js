c2dc9303e7a62d6c84e5084c0dc0282fbb39c8f1
Jimmy Gozal <jgozal@developerkit.co>
2017-06-13 19:55:35 +0200

    fix data

--- utils/sms.js
+++ utils/sms.js
@@ -10,7 +10,7 @@ let sendSMS = function () {
     randomCode = Math.floor(1000 + Math.random() * 9000);
     client.messages.create({
         body: 'Your code is ' + randomCode,
-        to: '+19293442441',  // Text this number
+        to: '+972526199424', // +19293442441  // Text this number
         from: '+15207292514' // From a valid Twilio number
     })
         .then((message) => console.log(message.sid));
