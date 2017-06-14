701059018d4f12c13a8721b14c98740655064bab
Jimmy Gozal <jgozal@developerkit.co>
2017-06-14 14:52:19 +0300

    inserting activity into db

--- api/requestHandlers.js
+++ api/requestHandlers.js
@@ -8,10 +8,35 @@ let auth = function (req, res) {
 
     if (req.body.auth === 1) {
 
+        let currentdate = new Date();
+        let entry_datetime = currentdate.getFullYear() + "-"
+            + (currentdate.getMonth() + 1) + "-"
+            + currentdate.getDate() + " "
+            + currentdate.getHours() + ":"
+            + currentdate.getMinutes() + ":"
+            + currentdate.getSeconds();
+
         con.query('SELECT DISTINCT uid, code, phone FROM entries WHERE code =' + req.body.code, function (err, results, fields) {
             if (err) console.log(err);
-            randomCode = sms.sendSMS(results[0].phone);
-            res.send(JSON.stringify({ result: 'orange' }));
+            if (results[0] !== undefined) {
+                randomCode = sms.sendSMS(results[0].phone);
+
+                let user = {
+                    uid: results[0].uid,
+                    entry_datetime: entry_datetime,
+                    code: results[0].code,
+                    phone: results[0].phone
+                }
+
+                con.query('INSERT INTO entries SET ?', user, function (err, results, fields) {
+                    if (err) console.log(err);
+                });
+
+                res.send(JSON.stringify({ result: 'orange' }));
+            } else {
+                res.send(JSON.stringify({ result: 'red' }));
+                randomCode = undefined; //reset
+            }
         });
 
     } else if (req.body.auth === 2 && req.body.code === randomCode) {
