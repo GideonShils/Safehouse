283f5835f617680b14b8b02a0ad28f53227c54ed
Jimmy Gozal <jgozal@developerkit.co>
2017-06-14 11:17:57 +0200

    sql fixed

--- ML/load_test_data.sql
+++ ML/load_test_data.sql
@@ -1,6 +1,6 @@
 load data local infile '/home/jgozal/Desktop/repos/safehouse/ML/training_data.csv' into table entries fields terminated by ','
   enclosed by '"'
   escaped by '"'
-  lines terminated by '\n'
+  lines terminated by '\r\n'
   ignore 1 lines
     (uid, entry_datetime, anomalous_bool, train_bool, phone);
